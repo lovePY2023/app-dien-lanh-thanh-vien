05ee3878b7a9d41c1e256f3f75b38e1fbce5b7f1
lovePY2023 <135207928+lovePY2023@users.noreply.github.com>
2026-03-23 15:20:47 +0700

    Change SUPABASE_ANON_KEY in config.js
    Updated SUPABASE_ANON_KEY with a new value.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const CONFIG = {
     SUPABASE_URL: "https://vkduoktamuqtevplkrax.supabase.co",
-    SUPABASE_ANON_KEY: "sb_publishable_NyP3F3ue8fBoShPfq1ADlA_JFGtZnlK", // Dán mã cực dài vào đây
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZHVva3RhbXVxdGV2cGxrcmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNDk5NzIsImV4cCI6MjA4OTgyNTk3Mn0.r5e_qDlc8lwimEyqHL-8lBGjuYVMcaNZTxGU0IwfIVs", // Dán mã cực dài vào đây
     TABLE_NAME: "DATA-KHACH-HANG"
 };
